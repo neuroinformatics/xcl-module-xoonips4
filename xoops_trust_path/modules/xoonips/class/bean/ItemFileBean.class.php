c1d1ce467dc47a94352c26f0aaf5d55ba35f62ff
Yoshihiro OKUMURA <orrisroot@gmail.com>
2020-09-28 23:33:48 +0900

    check sql queries.

--- xoops_trust_path/modules/xoonips/class/bean/ItemFileBean.class.php
+++ xoops_trust_path/modules/xoonips/class/bean/ItemFileBean.class.php
@@ -27,7 +27,7 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
      */
     public function countFile()
     {
-        $sql = 'SELECT COUNT(*) AS cnt FROM '.$this->table;
+        $sql = 'SELECT COUNT(*) AS `cnt` FROM `'.$this->table.'`';
         $result = $this->execute($sql);
         if (!$result) {
             return 0;
@@ -36,46 +36,6 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
         $this->freeRecordSet($result);
 
         return $row['cnt'];
-    }
-
-    /**
-     * get total file size.
-     *
-     * @param
-     *
-     * @return int
-     */
-    public function getTotalSize()
-    {
-        $sql = 'SELECT SUM(file_size) FROM '.$this->table;
-        $result = $this->execute($sql);
-        if (!$result) {
-            return 0;
-        }
-        $row = $this->fetchArray($result);
-        $this->freeRecordSet($result);
-
-        return $row;
-    }
-
-    /**
-     * get total file size.
-     *
-     * @param int $indexId:index_id
-     *
-     * @return int
-     */
-    public function getSizeByItem($itemId)
-    {
-        $sql = 'SELECT SUM(file_size) FROM '.$this->table.'WHERE item_id= '.$itemId;
-        $result = $this->execute($sql);
-        if (!$result) {
-            return 0;
-        }
-        $row = $this->fetchArray($result);
-        $this->freeRecordSet($result);
-
-        return $row;
     }
 
     /**
@@ -93,8 +53,8 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
         } else {
             $group_id = Xoonips_Utils::convertSQLNum($file['group_id']);
         }
-        $sql = "INSERT INTO $this->table (item_id,item_field_detail_id,original_file_name,mime_type,file_size,handle_name,";
-        $sql .= 'caption,sess_id,search_module_name,search_module_version,timestamp,download_count,occurrence_number,group_id)';
+        $sql = 'INSERT INTO `'.$this->table.'` (`item_id`, `item_field_detail_id`, `original_file_name`, `mime_type`, `file_size`, `handle_name`,';
+        $sql .= ' `caption`, `sess_id`, `search_module_name`, `search_module_version`, `timestamp`, `download_count`, `occurrence_number`, `group_id`)';
         $sql .= ' VALUES ('.Xoonips_Utils::convertSQLNum($file['item_id']);
         $sql .= ','.Xoonips_Utils::convertSQLNum($file['item_field_detail_id']).','.Xoonips_Utils::convertSQLStr($file['original_file_name']);
         $sql .= ','.Xoonips_Utils::convertSQLStr($file['mime_type']).','.Xoonips_Utils::convertSQLNum($file['file_size']);
@@ -138,12 +98,12 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
      */
     public function insertUploadFile($file, &$insertId)
     {
-        $sql = "INSERT INTO $this->table (item_id,item_field_detail_id,original_file_name,mime_type,file_size,sess_id,search_module_name,search_module_version,group_id)";
+        $sql = 'INSERT INTO `'.$this->table.'` (`item_id`, `item_field_detail_id`, `original_file_name`, `mime_type`, `file_size`, `sess_id`, `search_module_name`, `search_module_version`, `group_id`, `timestamp`)';
         $sql .= ' VALUES ('.Xoonips_Utils::convertSQLNum($file['item_id']).','.Xoonips_Utils::convertSQLNum($file['item_field_detail_id']);
         $sql .= ','.Xoonips_Utils::convertSQLStr($file['original_file_name']).','.Xoonips_Utils::convertSQLStr($file['mime_type']);
         $sql .= ','.Xoonips_Utils::convertSQLNum($file['file_size']).','.Xoonips_Utils::convertSQLStr($file['sess_id']);
         $sql .= ','.Xoonips_Utils::convertSQLStr($file['search_module_name']).','.Xoonips_Utils::convertSQLStr($file['search_module_version']);
-        $sql .= ','.Xoonips_Utils::convertSQLNum($file['group_id']).')';
+        $sql .= ','.Xoonips_Utils::convertSQLNum($file['group_id']).', 0)';
 
         $result = $this->execute($sql);
         if (!$result) {
@@ -165,19 +125,19 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     public function updateFile($fileId, $file)
     {
         $ret = true;
-        $sql = "UPDATE $this->table SET item_id=".Xoonips_Utils::convertSQLNum($file['item_id']);
-        $sql = $sql.',item_field_detail_id='.Xoonips_Utils::convertSQLNum($file['item_field_detail_id']);
-        $sql = $sql.',original_file_name='.Xoonips_Utils::convertSQLStr($file['original_file_name']);
-        $sql = $sql.',mime_type='.Xoonips_Utils::convertSQLStr($file['mime_type']);
-        $sql = $sql.',file_size='.Xoonips_Utils::convertSQLNum($file['file_size']);
-        $sql = $sql.',handle_name='.Xoonips_Utils::convertSQLStr($file['handle_name']);
-        $sql = $sql.',caption='.Xoonips_Utils::convertSQLStr($file['caption']);
-        $sql = $sql.',sess_id='.Xoonips_Utils::convertSQLStr($file['sess_id']);
-        $sql = $sql.',search_module_name='.Xoonips_Utils::convertSQLStr($file['search_module_name']);
-        $sql = $sql.',search_module_version='.Xoonips_Utils::convertSQLNum($file['search_module_version']);
-        $sql = $sql.',timestamp='.Xoonips_Utils::convertSQLNum($file['timestamp']);
-        $sql = $sql.',download_count='.Xoonips_Utils::convertSQLNum($file['download_count']);
-        $sql = $sql.' WHERE file_id='.$fileId;
+        $sql = 'UPDATE `'.$this->table.'` SET `item_id`='.Xoonips_Utils::convertSQLNum($file['item_id']);
+        $sql .= ',`item_field_detail_id`='.Xoonips_Utils::convertSQLNum($file['item_field_detail_id']);
+        $sql .= ',`original_file_name`='.Xoonips_Utils::convertSQLStr($file['original_file_name']);
+        $sql .= ',`mime_type`='.Xoonips_Utils::convertSQLStr($file['mime_type']);
+        $sql .= ',`file_size`='.Xoonips_Utils::convertSQLNum($file['file_size']);
+        $sql .= ',`handle_name`='.Xoonips_Utils::convertSQLStr($file['handle_name']);
+        $sql .= ',`caption`='.Xoonips_Utils::convertSQLStr($file['caption']);
+        $sql .= ',`sess_id`='.Xoonips_Utils::convertSQLStr($file['sess_id']);
+        $sql .= ',`search_module_name`='.Xoonips_Utils::convertSQLStr($file['search_module_name']);
+        $sql .= ',`search_module_version`='.Xoonips_Utils::convertSQLNum($file['search_module_version']);
+        $sql .= ',`timestamp`='.Xoonips_Utils::convertSQLNum($file['timestamp']);
+        $sql .= ',`download_count`='.Xoonips_Utils::convertSQLNum($file['download_count']);
+        $sql .= ' WHERE `file_id`='.intval($fileId);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -202,20 +162,20 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
         } else {
             $group_id = Xoonips_Utils::convertSQLNum($file['group_id']);
         }
-        $sql = "UPDATE $this->table SET item_id=".Xoonips_Utils::convertSQLNum($file['item_id']);
-        $sql = $sql.',item_field_detail_id='.Xoonips_Utils::convertSQLNum($file['item_field_detail_id']);
-        $sql = $sql.',original_file_name='.Xoonips_Utils::convertSQLStr($file['original_file_name']);
-        $sql = $sql.',mime_type='.Xoonips_Utils::convertSQLStr($file['mime_type']);
-        $sql = $sql.',file_size='.Xoonips_Utils::convertSQLNum($file['file_size']);
-        $sql = $sql.',handle_name='.Xoonips_Utils::convertSQLStr($file['handle_name']);
-        $sql = $sql.',caption='.Xoonips_Utils::convertSQLStr($file['caption']);
-        $sql = $sql.',sess_id='.Xoonips_Utils::convertSQLStr($file['sess_id']);
-        $sql = $sql.',search_module_name='.Xoonips_Utils::convertSQLStr($file['search_module_name']);
-        $sql = $sql.',search_module_version='.Xoonips_Utils::convertSQLNum($file['search_module_version']);
-        $sql = $sql.',timestamp='.Xoonips_Utils::convertSQLNum($file['timestamp']);
-        $sql = $sql.',download_count='.Xoonips_Utils::convertSQLNum($file['download_count']);
-        $sql = $sql.',occurrence_number='.Xoonips_Utils::convertSQLNum($file['occurrence_number']);
-        $sql = $sql.' WHERE file_id='.$fileId.' AND group_id = '.$group_id;
+        $sql = 'UPDATE `'.$this->table.'` SET `item_id`='.Xoonips_Utils::convertSQLNum($file['item_id']);
+        $sql .= ',`item_field_detail_id`='.Xoonips_Utils::convertSQLNum($file['item_field_detail_id']);
+        $sql .= ',`original_file_name`='.Xoonips_Utils::convertSQLStr($file['original_file_name']);
+        $sql .= ',`mime_type`='.Xoonips_Utils::convertSQLStr($file['mime_type']);
+        $sql .= ',`file_size`='.Xoonips_Utils::convertSQLNum($file['file_size']);
+        $sql .= ',`handle_name`='.Xoonips_Utils::convertSQLStr($file['handle_name']);
+        $sql .= ',`caption`='.Xoonips_Utils::convertSQLStr($file['caption']);
+        $sql .= ',`sess_id`='.Xoonips_Utils::convertSQLStr($file['sess_id']);
+        $sql .= ',`search_module_name`='.Xoonips_Utils::convertSQLStr($file['search_module_name']);
+        $sql .= ',`search_module_version`='.Xoonips_Utils::convertSQLNum($file['search_module_version']);
+        $sql .= ',`timestamp`='.Xoonips_Utils::convertSQLNum($file['timestamp']);
+        $sql .= ',`download_count`='.Xoonips_Utils::convertSQLNum($file['download_count']);
+        $sql .= ',`occurrence_number`='.Xoonips_Utils::convertSQLNum($file['occurrence_number']);
+        $sql .= ' WHERE `file_id`='.intval($fileId).' AND `group_id`='.intval($group_id);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -234,7 +194,7 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     public function deleteFiles($itemId)
     {
         $ret = true;
-        $sql = "DELETE FROM $this->table WHERE item_id=".$itemId;
+        $sql = 'DELETE FROM `'.$this->table.'` WHERE `item_id`='.intval($itemId);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -253,7 +213,7 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     public function getFile($fileId)
     {
         $ret = [];
-        $sql = "SELECT * FROM $this->table WHERE file_id=".$fileId;
+        $sql = 'SELECT * FROM `'.$this->table.'` WHERE `file_id`='.intval($fileId);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -276,7 +236,7 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     public function getFiles()
     {
         $ret = [];
-        $sql = "SELECT * FROM $this->table";
+        $sql = 'SELECT * FROM `'.$this->table.'`';
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -303,10 +263,10 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
 
         $g_sql = '';
         if (!is_null($group_id)) {
-            $g_sql = ' AND group_id = '.$group_id;
+            $g_sql = ' AND `group_id`='.intval($group_id);
         }
 
-        $sql = "SELECT * FROM $this->table WHERE item_id=".$itemId.$g_sql;
+        $sql = 'SELECT * FROM `'.$this->table.'` WHERE `item_id`='.intval($itemId).$g_sql;
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -331,10 +291,12 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
         $ret = 0;
         $tblIndex = $this->prefix($this->modulePrefix('index'));
         $tblLink = $this->prefix($this->modulePrefix('index_item_link'));
-        $sql = "SELECT SUM(c.file_size) AS sum FROM $this->table c WHERE c.item_id IN";
-        $sql .= " (SELECT b.item_id FROM $tblIndex a,$tblLink b";
-        $sql .= " WHERE a.groupid=$groupId AND a.open_level=2 AND a.index_id=b.index_id AND b.certify_state>=2";
-        $sql .= " AND b.item_id NOT IN (SELECT item_id FROM $tblIndex d,$tblLink e WHERE d.open_level=1 AND d.index_id=e.index_id AND e.certify_state>=2))";
+        $sql = 'SELECT SUM(`c`.`file_size`) AS `sum` FROM `'.$this->table.'` `c` WHERE `c`.`item_id` IN'
+            .' (SELECT `b`.`item_id` FROM `'.$tblIndex.'` `a`,`'.$tblLink.'` `b`'
+            .'   WHERE `a`.`groupid`='.intval($groupId).' AND `a`.`open_level`=2 AND `a`.`index_id`=`b`.index_id` AND `b`.`certify_state`>=2'
+            .'   AND `b`.`item_id` NOT IN (SELECT `item_id` FROM `'.$tblIndex.'` `d`, `'.$tblLink.'` `e` WHERE `d`.`open_level`=1 AND `d`.`index_id`=`e`.`index_id` AND `e`.`certify_state`>=2'
+            .' )'
+            .')';
         $result = $this->execute($sql);
         if (!$result) {
             return 0;
@@ -360,9 +322,8 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     {
         $indexTable = $this->prefix($this->modulePrefix('index_item_link'));
         $userTable = $this->prefix($this->modulePrefix('item_users_link'));
-        $sql = 'SELECT sum(a.file_size) AS sizes FROM '.$this->table.' a INNER JOIN '.$userTable.' b ON a.item_id=b.item_id '
-        .' WHERE b.uid='.$uid
-        .' AND b.item_id NOT IN (SELECT item_id FROM '.$indexTable.' WHERE certify_state>=2)';
+        $sql = 'SELECT SUM(`a`.`file_size`) AS `sizes` FROM `'.$this->table.'` `a` INNER JOIN `'.$userTable.'` `b` ON `a`.`item_id`=`b`.`item_id`'
+            .' WHERE `b`.`uid`='.intval($uid).' AND `b`.`item_id` NOT IN (SELECT `item_id` FROM `'.$indexTable.'` WHERE `certify_state`>=2)';
         $result = $this->execute($sql);
         if (!$result) {
             return 0;
@@ -377,24 +338,6 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     }
 
     /**
-     * update download count.
-     *
-     * @param int $fileId:file_id
-     *
-     * @return bool true:success,false:failed
-     */
-    public function updateDownloadCount($fileId)
-    {
-        $sql = 'UPDATE '.$this->table.' SET download_count=download_count+1 WHERE file_id='.$fileId;
-        $result = $this->execute($sql);
-        if (!$result) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
      * delete file.
      *
      * @param int $fileId:file id
@@ -403,7 +346,7 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
      */
     public function delete($fileId)
     {
-        $sql = 'DELETE FROM '.$this->table.' WHERE file_id='.$fileId;
+        $sql = 'DELETE FROM `'.$this->table.'` WHERE `file_id`='.intval($fileId);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -423,10 +366,10 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     {
         $tblDetail = $this->prefix($this->modulePrefix('item_field_detail'));
         $tblViewtype = $this->prefix($this->modulePrefix('view_type'));
-        $sql = "SELECT c.name FROM ($this->table a LEFT JOIN $tblDetail b";
-        $sql = $sql.' ON (a.item_field_detail_id=b.item_field_detail_id)) ';
-        $sql = $sql." LEFT JOIN $tblViewtype c ON (c.view_type_id=b.view_type_id)";
-        $sql = $sql.' WHERE a.file_id='.$fileId;
+        $sql = 'SELECT c.name FROM `'.$this->table.'` `a`'
+            .' LEFT JOIN `'.$tblDetail.'` `b` ON (`a`.`item_field_detail_id`=`b`.`item_field_detail_id`))'
+            .' LEFT JOIN `'.$tblViewtype.'` `c` ON (`c`.`view_type_id`=`b`.`view_type_id`)'
+            .' WHERE `a`.`file_id`='.intval($fileId);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -450,8 +393,8 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     public function getFileInformation($fileId)
     {
         $ret = [];
-        $sql = 'SELECT original_file_name,file_size,mime_type,timestamp,download_count ';
-        $sql .= "FROM $this->table WHERE file_id=".$fileId;
+        $sql = 'SELECT `original_file_name`, `file_size`, `mime_type`, `timestamp`, `download_count`';
+        $sql .= ' FROM '.$this->table.' WHERE `file_id`='.intval($fileId);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -476,7 +419,7 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
     public function getFilesByDetailId($item_detail_id, $item_id, $group_id = 0)
     {
         $ret = [];
-        $sql = "SELECT * FROM $this->table WHERE item_field_detail_id= ${item_detail_id} AND item_id = ${item_id} AND group_id = ${group_id}";
+        $sql = 'SELECT * FROM `'.$this->table.'` WHERE `item_field_detail_id`='.intval($item_detail_id).' AND `item_id`='.intval($item_id).' AND `group_id`='.intval($group_id);
         $result = $this->execute($sql);
         if (!$result) {
             return false;
@@ -485,33 +428,6 @@ class Xoonips_ItemFileBean extends Xoonips_BeanBase
             $ret[] = $row;
         }
         $this->freeRecordSet($result);
-
-        return $ret;
-    }
-
-    /**
-     * item_id exist in item_file,then item_field_detail.
-     *
-     * @param type $item_id
-     *
-     * @return type
-     */
-    public function getFileExistDetailByItem($item_id)
-    {
-        $ret = [];
-        $sql = 'SELECT file.*,table_name FROM '.
-            $this->table.' as file,'.
-            $this->detailtable.' as detail '.
-            'WHERE file.item_field_detail_id = detail.item_field_detail_id AND '.
-            'detail.released = 1 AND '.
-            'item_id = '.$item_id;
-        $result = $this->execute($sql);
-        if ($result) {
-            while ($row = $this->fetchArray($result)) {
-                $ret[] = $row;
-            }
-            $this->freeRecordSet($result);
-        }
 
         return $ret;
     }
